2b0e9a642321c30efc636b7408a23722ac2af089
Jodie Alaine Parker <me@jodiealaine.com>
2015-08-04 17:18:55 +0100

    Added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,14 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile
   end
+  def create
+  	@user = User.find( params[:user_id] )
+  	@profile = @user.build_profile(profile_params)
+  	if @profile.save
+  		flash[:sucess] = "Profile updated!"
+  		redirect_to user_path( params[:user_id] )
+  	else
+  		render action: :new
+  	end
+  end
 end
