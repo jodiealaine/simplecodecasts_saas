5e4e0cfee2fa428c523e2ed5bf63027b8b9c8213
Jodie Alaine Parker <me@jodiealaine.com>
2015-08-04 17:24:43 +0100

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,4 +14,9 @@ class ProfilesController < ApplicationController
   		render action: :new
   	end
   end
+
+  private
+  	def profile_params
+  		params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  	end
 end
